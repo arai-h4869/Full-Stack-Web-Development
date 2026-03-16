40218486829b6870314bb2579551bc01445f0051
arai-h4869 <77153197+arai-h4869@users.noreply.github.com>
2026-03-16 15:55:09 +0000

    feat: add GET PUT DELETE inventory/products/[id]

--- backend/api/inventory/views.py
+++ backend/api/inventory/views.py
@@ -1,4 +1,5 @@
 from rest_framework import status
+from rest_framework.exceptions import NotFound
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.viewsets import ModelViewSet
@@ -12,15 +13,25 @@ class ProductView(APIView):
     商品操作に関する関数
     """
 
-    def get(self, request, format=None):
+    def get_object(self, pk):
+        try:
+            return Product.objects.get(pk=pk)
+        except Product.DoesNotExist:
+            raise NotFound
+
+    def get(self, request, id=None, format=None):
         """
         商品の一覧を取得する
         """
-        queryset = Product.objects.all()
-        serializer = ProductSerializer(queryset, many=True)
+        if id is None:
+            queryset = Product.objects.all()
+            serializer = ProductSerializer(queryset, many=True)
+        else:
+            product = self.get_object(id)
+            serializer = ProductSerializer(product)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
-    def post(self, request, formant=None):
+    def post(self, request, format=None):
         """
         商品を登録する
         """
@@ -29,6 +40,24 @@ class ProductView(APIView):
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    def put(self, request, id, format=None):
+        """
+        商品を更新する
+        """
+        product = self.get_object(id)
+        serializer = ProductSerializer(instance=product, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def delete(self, request, id, format=None):
+        """
+        商品を削除する
+        """
+        product = self.get_object(id)
+        product.delete()
+        return Response(status=status.HTTP_200_OK)
 
 
 class ProductModelView(ModelViewSet):
